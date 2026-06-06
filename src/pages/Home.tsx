ec391f18da6a63f0851d82364ab5ac37b752674f
mgonzalezp <mgonzalezp@fi.uba.ar>
2026-06-06 15:18:09 -0300

    add game access to landing page

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -5,9 +5,14 @@ export default function Home() {
   return (
     <div className="hero">
       <img src={heroLogoSrc} alt="Misa Ricotera" className="hero-logo" />
-      <Link to="/discografia" className="hero-btn">
-        Ver Discografía
-      </Link>
+      <div className="hero-actions">
+        <Link to="/discografia" className="hero-btn">
+          Ver Discografía
+        </Link>
+        <Link to="/ultima-misa" className="hero-btn hero-btn--misa">
+          La última misa
+        </Link>
+      </div>
     </div>
   );
 }
